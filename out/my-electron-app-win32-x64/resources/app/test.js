c9e53eee89b344a8180468e0ff57b1ce265130c9
gwrxuk <liujunghua@reddotpay.com>
2021-06-01 18:26:30 +0800

    web player

---
+++ out/my-electron-app-win32-x64/resources/app/test.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e5c416e39c2e689ed4353bc2d8adb0832a14289420c6ab4bde8d622d991ee321
+size 6
---
+++ script_local.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f04c6e2dcb6ddbf57232d4fd07877ed206cc288c4e4411ffb03b484e2a5fc34d
+size 201
