2d481cc24ef0c1d64d635b950b883fa1a0b3c2cc
gwrxuk <liujunghua@reddotpay.com>
2021-06-02 22:37:39 +0800

    full screen

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:602b9d5459c4e78533b51e152a8bfbcee0348e716359890bf43af0a5a321deb0
-size 737
+oid sha256:33b6cd56e65b06fe64122e379d35ba3a299fe2ad07cc6fd44a7eaf3d568bdcf2
+size 758
--- out/my-electron-app-win32-x64/resources/app/main.js
+++ out/my-electron-app-win32-x64/resources/app/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:602b9d5459c4e78533b51e152a8bfbcee0348e716359890bf43af0a5a321deb0
-size 737
+oid sha256:769439d18494764260b8a975720199f9d8d753ce12a135c7eb4f2a5aef278940
+size 735
