aa773eb98ebdddf300d0aadfdc9e449cee6baa4b
gwrxuk <liujunghua@reddotpay.com>
2021-06-02 23:26:43 +0800

    icon

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33b6cd56e65b06fe64122e379d35ba3a299fe2ad07cc6fd44a7eaf3d568bdcf2
-size 758
+oid sha256:f2775b183c632280c4b9423f04fa196b44cadb61c639a4c3f6786347c7a4ae1d
+size 880
